f7a99703eb34ceaf32c5e3e0b03d0e133e5fa3e4
Boris Glass <github@com.glass.li>
2018-06-09 09:00:44 +0200

    style

--- app/javascript/elm-src/Node.elm
+++ app/javascript/elm-src/Node.elm
@@ -1,19 +1,21 @@
-module Node exposing (..)
-import Element exposing (..)
-import Element.Attributes exposing (..)
+module Node exposing (viewTree)
+
+import Element as E exposing (Element)
+-- import Element.Attributes exposing (..)
 import Html as H exposing (Html)
+import Html.Attributes as HA
 import Model exposing (..)
 import View.Style exposing (..)
 import Msg   exposing  (Msg(..))
 
 viewTree : Node -> Element Styles variation Msg
 viewTree node =
-  Element.html (viewNode node)
+  E.html (viewNode node)
 
 viewNode : Node -> Html Msg
 viewNode node =
   uli
-    ( [H.text node.name]
+    ( [item node]
         ++ (node |> children |> List.map viewNode )
     )
 
@@ -26,3 +28,19 @@ children node =
   case node.children of
     Children  Nothing        -> []
     Children (Just children) -> children
+
+item : Node -> Html msg
+item node =
+  H.div [style node] [text node]
+
+
+style : Node -> H.Attribute msg
+style node =
+  HA.style
+    [ ("font-weight", "bold")
+    , ("color", "blue")
+    ]
+
+text : { a | name : String } -> Html msg
+text node =
+  H.text node.name
